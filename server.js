6078ae2aeecfe053ba1e39eacc9944dde52c2667
Gan <camotoad1@gmail.com>
2021-11-24 16:52:38 -0500

    create db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const configExpress = () => {
     const app = express();
@@ -11,10 +12,42 @@ const configExpress = () => {
     return app;
 };
 
+const db = mysql.createConnection({
+    host        : 'localhost',
+    user        : 'root',
+    password    : ''
+    ,database    : 'node_challenge_login_db' //Comment out and create db first
+});
+
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('Mysql Connected');
+})
+
+//create db
+// function createDB(){
+//     let q = 'create database node_challenge_login_db';
+//     db.query(q, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         console.log('DB created');
+//     });
+
+//     let q2 = 'create table node_challenge_login_db.users(id int auto_increment, display_name varchar(100), password varchar(100), email varchar(100), picture varchar(100), primary key (id))';
+//     db.query(q2, (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//         console.log('Table created');
+//     });
+// };
+
 const app = configExpress();
 
 app.listen('3000', () => {
     console.log('Server running at http://localhost:3000/');
+    //createDB();
 });
 
 module.exports = app;
